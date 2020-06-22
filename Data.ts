7e105b4a584103b586566460c444628c0985548c
Sebastian Inman <sebastian@nicindustries.com>
2020-06-22 12:06:44 -0700

    Added data class

---
+++ Data.ts
@@ -0,0 +1,41 @@
+/**
+ * @class Data
+ * @description TypeScript class for manipulating data objects.
+ */
+
+export default class Data {
+
+
+  /**
+   * @method Data.Merge
+   * @description Merges two data objects together into one object.
+   * @param map
+   * @param data
+   * @constructor
+   */
+
+  static Merge(map: {}, data: {}) {
+    return {...map, ...data};
+  }
+
+
+  /**
+   * @method Data.Filter
+   * @description Filters a data object by key names.
+   * @param data {}
+   * @param whitelist []
+   *
+   * @example
+   * let data = {id: 1, name: 'test', domain: 'test.com'};
+   * let filter = Data.Filter(data, ['domain', 'name']);
+   */
+
+  static Filter(data: any, whitelist: any) {
+    return Object.keys(data).filter(key =>
+      whitelist.includes(key)).reduce((obj: {}, key: string) => {
+      obj[key] = data[key];
+      return obj;
+    }, {});
+  }
+
+}
